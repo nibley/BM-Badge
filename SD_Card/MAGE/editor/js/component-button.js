6771d20a35dbc6b1c6c933046ff0ca5b971351fa
nibley <5225323+nibley@users.noreply.github.com>
2024-07-13 04:36:50 -0600

    create component-button for outlined buttons to be drier

---
+++ SD_Card/MAGE/editor/js/component-button.js
@@ -0,0 +1,46 @@
+var componentButtonTextMap = {
+	delete: 'X',
+};
+var componentButtonoutlineClassMap = {
+	delete: 'btn-outline-danger',
+};
+
+vueComponents['component-button'] = {
+	name: 'component-button',
+	props: {
+		type: {
+			type: String,
+			// default: 'light',
+		},
+		color: {
+			type: String,
+			// default: null,
+		},
+	},
+	setup: function(props) {
+		var outlineClass = Vue.computed(function() {
+			if(props.color) {
+				return '';
+			}
+			return componentButtonoutlineClassMap[props.type] || 'btn-outline-light';
+		});
+
+		var text = Vue.computed(function() {
+			return componentButtonTextMap[props.type] || '_';
+		});
+
+		return {
+			// computeds:
+			outlineClass,
+			text,
+		};
+	},
+	template: /*html*/`
+<button
+	type="button"
+	class="btn"
+	:class="outlineClass"
+>
+	<slot>{{ text }}</slot>
+</button>
+`};
